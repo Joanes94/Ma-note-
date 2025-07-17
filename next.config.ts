f8188bfa97fc8a3d0e955c0881983000ac4fbfb6
Joanes94 <136589295+Joanes94@users.noreply.github.com>
2025-07-17 13:00:34 +0100

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  allowedDevOrigins: ['http://10.5.0.251:3000'], // remplace par l'IP locale utilisée sur mobile
+}
+
+module.exports = nextConfig
